a3b56c756c944023455486711603ccea58b94e38
tobia1k <113366743+tobia1k@users.noreply.github.com>
2024-09-02 19:01:01 -0400

    Complete exercise 5
    Get the sum of all number from and including the arguments passed
    to the function. A little sloppier than the solution given. typeof
    is redundant in the first if statement. first probably shouldn't be
    the value that changes in the for loop (more like let i = first). It
    works fine as is but would be more readable.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    if (typeof first === "number" && typeof last === "number" && 
+        first >= 0 && last >= 0 && 
+        Number.isInteger(first) && Number.isInteger(last)){
+        if (first != Math.min(first, last)) {
+            const tmp = first;
+            first = last;
+            last = tmp;
+        }
+        let sum = 0;
+        for (first; first <= last; first++) {
+            sum += first;
+        }
+        return sum;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
