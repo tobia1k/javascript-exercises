9c70fe8443fc5d2c2f391a3a227f210ff26a8cf6
tobia1k <113366743+tobia1k@users.noreply.github.com>
2024-11-23 17:50:18 -0500

    Compelete JavaScript Exercise 12
    This is the final JavaScript exercise of the Foundations Course.
    I used the reduce method to find the oldest person out of an array
    of people objects. The calculateAge helper functions can find the
    person's age either by subtracting the yearOfBirth from the
    yearOfDeath or by subtracting the yearOfBirth from the current year
    if there is no given yearOfDeath. The reduce function returns the
    currentPerson if currentAge is greater than oldestAge and oldest
    otherwise.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(person) {
+    return person.reduce((oldest, currentPerson) => {
 
+        let currentAge = 0;
+        let oldestAge = 0;
+
+        currentAge = calculateAge(currentPerson);
+        oldestAge = calculateAge(oldest);
+
+        return currentAge > oldestAge ? currentPerson : oldest;
+    }, person[0]);
 };
+
+function calculateAge(person) {
+    if (person.yearOfDeath === undefined) {
+        const d = new Date();
+        let currentYear = d.getFullYear();
+        return currentYear - person.yearOfBirth;
+    }
+    else {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
