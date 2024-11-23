8f47daed95b099799ced87564814dad8f05e0bb5
tobia1k <113366743+tobia1k@users.noreply.github.com>
2024-11-23 16:16:42 -0500

    Complete JavaScript Exercises 09 and 10
    In exercise 09, I made the input string all lowercase in order
    to be case insensitive. I then split the string so I could then
    filter it with the formatStr function which simply checks the
    given character against the regex and returns true if the
    character is not whitespace or punctuation. The array gets
    rejoined into a string, now all lowercase and without any
    puncuation or whitespace. It then gets sliced into firstHalf
    and lastHalf. lastHalf gets reversed so that it (if the input is
    in fact a palindrome) matches firstHalf.
    
    In exercise 10, the function returns the nth fibonacci number.
    This is done with a simple for loop that adds the previous two
    numbers in the sequence.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    formattedStr = str.toLowerCase().split("").filter(formatStr).join("");
+    let firstHalf = formattedStr.slice(0, (Math.floor(formattedStr.length / 2)));
+    let lastHalf = formattedStr.slice(Math.ceil(formattedStr.length / 2)).split("").reverse().join("");
 
+    for (let i = 0; i < firstHalf.length; i++) {
+        if (firstHalf[i] !== lastHalf[i])
+            return false;
+    }
+    return true;
 };
+
+function formatStr(c) {
+    return !/[^\w\s]|\s/g.test(c);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i <= num; i++) {
+        let tmp = a;
+        a = b;
+        b += tmp;
+    }
+    return a;
 };
 
 // Do not edit below this line
